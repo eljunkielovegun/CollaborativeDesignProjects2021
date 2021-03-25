2375352c2b3d567113815a3e1f9a0056f81005d7
eljunkielovegun <el.kevo@gmail.com>
2021-03-25 15:43:23 -0400

    retry the map

---
+++ mapTest/src/script.js
@@ -0,0 +1,19 @@
+import mapboxgl from 'mapbox-gl'
+import * as THREE from 'three'
+
+//console.log(mapboxgl)
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5wYXR0b24xIiwiYSI6ImNrbTZsa3NsdjBwNmoyb3FzYzJidmU3Y2MifQ.Mk2dd2jdoi4Hkij0utyJwg';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+console.log(map)
+var mapDiv = document.getElementById('map');
+// console.log(mapDiv)
+
+
+//console.log(mapboxgl)
