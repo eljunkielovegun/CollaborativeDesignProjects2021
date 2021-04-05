c7ab9ee21e1737c524caf64a593a2e3764ffc153
eljunkielovegun <el.kevo@gmail.com>
2021-04-05 10:29:13 -0400

    update map resources

--- mapTest/src/script.js
+++ mapTest/src/script.js
@@ -2,16 +2,35 @@ import mapboxgl from 'mapbox-gl'
 import * as THREE from 'three'
 
 //console.log(mapboxgl)
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5wYXR0b24xIiwiYSI6ImNrbTZsa3NsdjBwNmoyb3FzYzJidmU3Y2MifQ.Mk2dd2jdoi4Hkij0utyJwg';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: [-95.7, 37.1], // starting position [lng, lat]
+    zoom: 4 // starting zoom
 });
 
-console.log(map)
+map.on('load', function () {
+    const mapContainer = document.getElementsByClassName("mapboxgl-canvas")
+    
+    mapContainer[0].style.height = '100vh'
+    map.resize()
+});
+ 
+
+
+
+    //console.log(mapContainer[0].style.height)
+
+
+const mapDiv = document.getElementById('map')
+mapDiv.style.height = sizes.height
+console.log(mapDiv)
 
 
 
